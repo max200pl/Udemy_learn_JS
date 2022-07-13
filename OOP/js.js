066b64603939d903588e206e4f2d88c684411921
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2022-07-13 19:41:46 +0300

    Base concept creating new Object in js

--- OOP/js.js
+++ OOP/js.js
@@ -1,5 +1,23 @@
-"use strict"
 
+// 1. New {} is created;
+// 2. function is called, this = {}
+// 3. {} linked to prototype
 
+"use strict";
+const Person = function (firstName, birthYear) {
+	// instance properties
+	this.firstName = firstName;
+	this.birthYear = birthYear;
 
+	// Never to this 
+	this.calcAge = function () {
+		console.log(2027 - this.birthYear);
+	};
+}
 
+const jonas = new Person("Jonas", 1991);
+const matilda = new Person('Matilda', 2017); // create instance Person class
+const jack = new Person('Jack', 1975)
+
+console.log(matilda, jack);
+console.log(jack instanceof Person);
