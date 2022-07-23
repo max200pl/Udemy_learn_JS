acc23776e0b231e64c05cd6632caca62cea1584b
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2022-07-23 18:24:08 +0300

    223 Encapsulation: Private Class Fiealds ant methods

--- OOP/ES6Calsses.js
+++ OOP/ES6Calsses.js
@@ -3,7 +3,11 @@
  * * Classes are not hoisted
  * * First-class citizen
  * * Classes are executed in strict mode
+ * * Not yet support real data privacy and encapsulation
+ * * 
  */
+
+//* C++ or Java properties are usually called fields. 
 
 // class expression
 const PersonCl = class { }
@@ -59,48 +63,77 @@ const walter = new PersonCL("Walter White", 2021);
 PersonCL.hey();
 
 
-/* Another Class example */
+/* Private class Fields and Methods */
+
+//* So in this proposal, there are actually four different kings:
+// * Public fields
+// * Private fields -> not accessible in the outside
+// * Public methods
+// * Privet methods 
+// * (there is also the static )
+
 
 class Account {
+	//Public fields (instances)
+	locale = navigator.language;  // public instance field
+
+	// Private fields (instances)
+	#movements = []; // not supposed to be touched outside of the class
+	#pin;
+
 	constructor(owner, currency, pin) {
 		this.owner = owner;
 		this.currency = currency;
-		this.pin = pin;
-		this.movements = [];
-		this.locale = navigator.language;
+		this.#pin = pin;
+		// protected property
+		// this._movements = []; 
+		// this.locale = navigator.language;
 
 		console.log(`Thanks for opening an account, ${owner}`);
 	}
 
-	//* Public interface
+	//* Public interface/methods
+	getMovements() {
+		return this.#movements;
+	}
+
 	deposit(val) {
-		this.movements.push(val)
+		this.#movements.push(val)
 	}
 
 	withdraw(val) {
 		this.deposit(-val)
 	}
 
-	approveLoan(val) {
-		return true;
-	}
-
 	requestLoan(val) {
-		if (this.approveLoan(val)) {
+		if (this.#approveLoan(val)) {
 			this.deposit(val);
 			console.log(`Loan approved`);
 		}
 	}
+
+	static helper() { //* not be available on all the instances, but only on the class itself.
+		console.log('This class');
+	}
+
+	// Private methods
+
+	#approveLoan(val) {
+		return true;
+	}
+
 }
 
 
 const acc1 = new Account('Jonas', 'EUR', 11111);
 
-//! acc1.movements.push(250) not using this. Need added new method
+//! acc1.#movements.push(250) not using this. Need added new method
 acc1.deposit(14);
 acc1.withdraw(15)
 
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+//acc1.#approveLoan(1000);// error
+console.log(acc1.getMovements());
 console.log(acc1);
 
+Account.helper()
