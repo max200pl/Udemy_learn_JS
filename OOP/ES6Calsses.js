3c5bfc70faf5091e2b194a0adb0a4ed8f4e9aaa4
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2022-07-23 13:57:27 +0300

    222  Another Class example

--- OOP/ES6Calsses.js
+++ OOP/ES6Calsses.js
@@ -58,3 +58,49 @@ const walter = new PersonCL("Walter White", 2021);
 
 PersonCL.hey();
 
+
+/* Another Class example */
+
+class Account {
+	constructor(owner, currency, pin) {
+		this.owner = owner;
+		this.currency = currency;
+		this.pin = pin;
+		this.movements = [];
+		this.locale = navigator.language;
+
+		console.log(`Thanks for opening an account, ${owner}`);
+	}
+
+	//* Public interface
+	deposit(val) {
+		this.movements.push(val)
+	}
+
+	withdraw(val) {
+		this.deposit(-val)
+	}
+
+	approveLoan(val) {
+		return true;
+	}
+
+	requestLoan(val) {
+		if (this.approveLoan(val)) {
+			this.deposit(val);
+			console.log(`Loan approved`);
+		}
+	}
+}
+
+
+const acc1 = new Account('Jonas', 'EUR', 11111);
+
+//! acc1.movements.push(250) not using this. Need added new method
+acc1.deposit(14);
+acc1.withdraw(15)
+
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1);
+
