2623e470e68bcbe7787eb523c439e55773b93238
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2022-07-26 09:06:13 +0300

    228 - 230 Intro

---
+++ Project_Mapty-OPP/js.js
@@ -0,0 +1,5 @@
+/**
+ * * User story: Description of the application's functionality from the user's perspective
+ * * Common format: As a [type of user], I want [an action] so that [a benefit]
+ * 
+ */
