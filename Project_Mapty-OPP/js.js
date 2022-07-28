f3d98780f97d7d3420e96423470a9285bf69515c
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2022-07-28 09:26:37 +0300

    228-233 Init progect Mapty (get current location in map)

--- Project_Mapty-OPP/js.js
+++ Project_Mapty-OPP/js.js
@@ -1,5 +1,4 @@
 /**
  * * User story: Description of the application's functionality from the user's perspective
  * * Common format: As a [type of user], I want [an action] so that [a benefit]
- * 
  */
